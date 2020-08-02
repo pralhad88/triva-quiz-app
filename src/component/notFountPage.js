cd4e4320e7618d57146c1c34551e6e11e3cc0c74
pralhad88 <pralhad@navgurukul.org>
2020-08-02 19:54:31 +0530

    color changed

--- src/component/notFountPage.js
+++ src/component/notFountPage.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => ( // if user enter wrong path on url then it redirect on not found page and give then lading page url option.
   <div className="container">
-    404 Page Not Found - <Link to="/">Go home</Link>
+    404 Page Not Found - <Link to="/" style={{color: 'black'}}>Go home</Link>
   </div>
 );
 
