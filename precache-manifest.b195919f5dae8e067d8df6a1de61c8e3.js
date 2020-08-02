67b219171b56a6b2f519deb789cf391d8985ac27
pralhad88 <pralhad@navgurukul.org>
2020-08-02 18:27:25 +0530

    Updates

---
+++ precache-manifest.b195919f5dae8e067d8df6a1de61c8e3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fec1213c768488f1681206865b596b9",
+    "url": "/triva-quiz-app/index.html"
+  },
+  {
+    "revision": "1e1c6a35a98537eda7fe",
+    "url": "/triva-quiz-app/static/css/main.ec8d30a3.chunk.css"
+  },
+  {
+    "revision": "4344667c0cf150f59dda",
+    "url": "/triva-quiz-app/static/js/2.72bc558d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/triva-quiz-app/static/js/2.72bc558d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e1c6a35a98537eda7fe",
+    "url": "/triva-quiz-app/static/js/main.9a5ff073.chunk.js"
+  },
+  {
+    "revision": "1111f0946b4aa294c977",
+    "url": "/triva-quiz-app/static/js/runtime-main.888fc139.js"
+  },
+  {
+    "revision": "7d41c607771fbce51bdb5a0e50aea505",
+    "url": "/triva-quiz-app/static/media/image.7d41c607.jpg"
+  },
+  {
+    "revision": "897f7004ca16167634ac844fa228576a",
+    "url": "/triva-quiz-app/static/media/right.897f7004.jpeg"
+  }
+]);
